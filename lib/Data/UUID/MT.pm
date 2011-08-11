df355bd4e053bcfe89c33cf0ebd55e6f579454c8
David Golden <dagolden@cpan.org>
2011-08-11 18:06:33 -0400

    initial import of Data::UUID::MT

---
+++ lib/Data/UUID/MT.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package Data::UUID::MT;
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+# ABSTRACT: No abstract given for Data::UUID::MT
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Data::UUID::MT;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
